4189f230c4bba50f713c2e1607c3b25134637f5f
taewoo <https://github.com/rlaxodn322>
2023-11-10 10:10:52 +0900

    first

--- MY-APP/routes.ts
+++ MY-APP/routes.ts
@@ -11,16 +11,16 @@ export const routes = [
     default: '/icons/sidebar/user_default.svg',
     name: 'Login',
   },
-  // {
-  //   router: '/',
-  //   active: '/icons/sidebar/led_active.svg',
-  //   default: '/icons/sidebar/led_default.svg',
-  //   name: 'text',
-  // },
-  // {
-  //   router: '/',
-  //   active: '/icons/sidebar/nutrient-pump_active.svg',
-  //   default: '/icons/sidebar/nutrient-pump_default.svg',
-  //   name: 'text',
-  // },
+  {
+    router: '/',
+    active: '/icons/sidebar/dashboard_active.svg',
+    default: '/icons/sidebar/dashboard_default.svg',
+    name: 'text',
+  },
+  {
+    router: '/',
+    active: '/icons/sidebar/user_active.svg',
+    default: '/icons/sidebar/user_default.svg',
+    name: 'text',
+  },
 ];
