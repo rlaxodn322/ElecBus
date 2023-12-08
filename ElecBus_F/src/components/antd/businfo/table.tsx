921f2d26e4bd03b7610bb0774fd6e6af8d39b55b
taewoo <rlaxodn322@gmail.com>
2023-12-08 13:51:32 +0900

    오류관리 페이지 수정중

--- ElecBus_F/src/components/antd/businfo/table.tsx
+++ ElecBus_F/src/components/antd/businfo/table.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'antd';
 
 const style: React.CSSProperties = {
-  background: 'white',
   padding: '8px 0',
   width: '150px',
   height: '150px',
@@ -14,7 +13,7 @@ const style: React.CSSProperties = {
   justifyContent: 'center',
   alignItems: 'center',
   textAlign: 'center',
-  color: '#333', // Dark gray color
+  fontSize: '18px',
 };
 
 const containerStyle: React.CSSProperties = {
@@ -23,36 +22,52 @@ const containerStyle: React.CSSProperties = {
   paddingTop: '20px',
 };
 
-const dummyDataVersions = [
-  [
-    { label: '운행상태', info: '운행 중' },
-    { label: '모터온도', info: '50°C' },
-    { label: '모터제어기온도', info: '60°C' },
-    { label: '에어펌프', info: '작동 중' },
-    { label: '배터리SOC', info: '80%' },
-    { label: '배터리최고온도', info: '45°C' },
-    { label: '배터리 최저온도', info: '25°C' },
-    { label: '배터리팩 충전상태', info: '충전 중' },
-    { label: '배터리팩 상세온도', info: '40°C' },
-    { label: '배터리 소화기 상태', info: '정상' },
-    { label: 'Low RPM', info: '1200' },
-    { label: 'High RPM', info: '3000' },
-  ],
-  [
-    { label: '운행상태', info: '주차 중' },
-    { label: '모터온도', info: '40°C' },
-    { label: '모터제어기온도', info: '55°C' },
-    { label: '에어펌프', info: '정지' },
-    { label: '배터리SOC', info: '60%' },
-    { label: '배터리최고온도', info: '40°C' },
-    { label: '배터리 최저온도', info: '20°C' },
-    { label: '배터리팩 충전상태', info: '미충전' },
-    { label: '배터리팩 상세온도', info: '35°C' },
-    { label: '배터리 소화기 상태', info: '이상' },
-    { label: 'Low RPM', info: '1000' },
-    { label: 'High RPM', info: '2500' },
-  ],
-];
+const generateDummyData = (version) => {
+  return [
+    { label: '운행상태', info: version % 2 === 0 ? '운행 중' : '주차 중' },
+    { label: '모터온도', info: `${Math.floor(Math.random() * 61) + 20}°C` },
+    { label: '모터제어기온도', info: `${Math.floor(Math.random() * 61) + 20}°C` },
+    { label: '에어펌프', info: version % 2 === 0 ? '작동 중' : '정지' },
+    { label: '배터리SOC', info: `${Math.floor(Math.random() * 61) + 20}%` },
+    { label: '배터리최고온도', info: `${Math.floor(Math.random() * 41) + 20}°C` },
+    { label: '배터리 최저온도', info: `${Math.floor(Math.random() * 21) + 20}°C` },
+    { label: '배터리팩 충전상태', info: version % 2 === 0 ? '충전 중' : '미충전' },
+    { label: '배터리팩 상세온도', info: `${Math.floor(Math.random() * 31) + 20}°C` },
+    { label: '배터리 소화기 상태', info: version % 2 === 0 ? '정상' : '이상' },
+    { label: 'Low RPM', info: `${Math.floor(Math.random() * 1001) + 1000}` },
+    { label: 'High RPM', info: `${Math.floor(Math.random() * 2001) + 1000}` },
+    // 전기차 관련 dummy data
+    { label: '충전 상태', info: version % 2 === 0 ? '충전 중' : '미충전' },
+    { label: '주행 거리', info: `${Math.floor(Math.random() * 201) + 100} km` },
+    { label: '배터리 용량', info: `${Math.floor(Math.random() * 21) + 30} kWh` },
+    { label: '충전소 거리', info: `${Math.floor(Math.random() * 101) + 50} km` },
+    // 추가적인 전기차 관련 dummy data
+    { label: '충전 시간', info: `${Math.floor(Math.random() * 6) + 1} 시간` },
+    { label: '평균 주행 속도', info: `${Math.floor(Math.random() * 51) + 50} km/h` },
+    { label: '배터리 수명', info: `${Math.floor(Math.random() * 501) + 500} 충전` },
+    { label: '과거 주행 기록', info: version % 2 === 0 ? '있음' : '없음' },
+    // 더 많은 전기차 관련 dummy data 추가 가능
+  ];
+};
+
+const dummyDataVersions = Array.from({ length: 10 }, (_, index) => generateDummyData(index));
+
+console.log(dummyDataVersions);
+
+const determineBackgroundColor = (label, info) => {
+  if (label === '운행상태' && info === '주차 중') {
+    return '#FF6347'; // Red
+  } else if (label === '운행상태' && info === '운행 중') {
+    return '#00FF7F'; // Green
+  } else if (label === '배터리 소화기 상태' && info === '이상') {
+    return '#FF6347'; // Red
+  } else if (label.includes('온도')) {
+    const temperature = parseInt(info);
+    return temperature > 40 ? '#FF6347' : '#00FF7F';
+  } else {
+    return '#00FF7F'; // Green
+  }
+};
 
 const App: React.FC = () => {
   const [currentVersionIndex, setCurrentVersionIndex] = useState(0);
@@ -60,22 +75,28 @@ const App: React.FC = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentVersionIndex((prevIndex) => (prevIndex === 0 ? 1 : 0));
-    }, 1000);
+    }, 500);
 
     return () => clearInterval(interval);
   }, []);
 
-  const currentVersionData = dummyDataVersions[currentVersionIndex];
+  const currentVersionData = generateDummyData(currentVersionIndex);
 
   return (
     <div style={containerStyle}>
       <Row gutter={1}>
         {currentVersionData.map((data, index) => (
-          <Col key={index} span={4}>
+          <Col key={index} span={2.5}>
             <div style={{ margin: '5px', width: 'max-content' }}>
-              <div style={style}>
+              <div
+                style={{
+                  ...style,
+                  background: determineBackgroundColor(data.label, data.info),
+                  color: determineBackgroundColor(data.label, data.info) === '#FF6347' ? 'white' : '#333',
+                }}
+              >
                 <div style={{ marginBottom: '8px', fontWeight: 'bold' }}>{data.label}</div>
-                <div style={{ fontSize: '18px' }}>{data.info}</div>
+                <div>{data.info}</div>
               </div>
             </div>
           </Col>
