00c8eb9f8d8ce395925cc231f0f576869c996bae
taewoo <rlaxodn322@gmail.com>
2024-01-02 17:09:40 +0900

    수정

--- ElecBus_F/src/config/config.ts
+++ ElecBus_F/src/config/config.ts
@@ -1,3 +1,3 @@
 export const backUrl =
   // process.env.NODE_ENV === 'production' ? 'http://smartfarm-back.firstcorea.com' : 'http://43.200.24.10:4000';
-  process.env.NODE_ENV === 'production' ? 'http://localhost:3090' : 'http://localhost:3000';
+  process.env.NODE_ENV === 'production' ? 'http://localhost:3080' : 'http://localhost:3000';
