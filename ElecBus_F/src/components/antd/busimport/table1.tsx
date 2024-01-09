89ae01ef74a3ec0883ac7741af93842001de3fb9
taewoo <rlaxodn322@gmail.com>
2024-01-09 14:01:10 +0900

    버스등록 수정중

--- ElecBus_F/src/components/antd/busimport/table1.tsx
+++ ElecBus_F/src/components/antd/busimport/table1.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import { Button, Form, Input, Select } from 'antd';
+import { Button, Form, Input, message } from 'antd';
 import type { FormInstance } from 'antd/es/form';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import axios from 'axios';
-const { Option } = Select;
 
 const layout = {
   labelCol: { span: 8 },
@@ -34,16 +33,22 @@ const App: React.FC = () => {
         serial,
       });
 
-      // 상태 업데이트
+      // 데이터베이스 저장이 완료된 후에 상태 업데이트
       setBusNumber(busNumber);
       setSerial(serial);
 
-      // 상태 업데이트 후 콘솔에 로그 찍기
+      // 콘솔에 로그 찍기
       console.log('데이터베이스에 저장 및 화면 업데이트 완료');
       console.log('현재 busNumber:', busNumber);
       console.log('현재 serial:', serial);
+
+      // 성공 메시지 표시
+      message.success('버스 등록이 성공적으로 완료되었습니다.');
     } catch (error) {
       console.error('데이터베이스 저장 중 오류:', error);
+
+      // 실패 메시지 표시
+      message.error('데이터베이스 저장 중 오류가 발생했습니다.');
     }
   };
 
@@ -58,19 +63,31 @@ const App: React.FC = () => {
       {/* Form 컴포넌트 시작 */}
       <Form {...layout} ref={formRef} name="control-ref" onFinish={onFinish} style={{ maxWidth: 600 }}>
         {/* 차량번호 입력 필드 */}
-        <Form.Item name="busNumber" label="차량번호" rules={[{ required: true }]}>
+        <Form.Item
+          name="busNumber"
+          label="차량번호"
+          rules={[
+            { required: true, message: '차량번호를 입력하세요.' },
+            { min: 3, max: 10, message: '차량번호는 3자에서 10자 사이여야 합니다.' },
+          ]}
+        >
           <Input style={inputStyle} placeholder="노선을 입력하세요." onChange={(e) => setBusNumber(e.target.value)} />
         </Form.Item>
-
         {/* 시리얼넘버 입력 필드 */}
-        <Form.Item name="serial" label="시리얼넘버" rules={[{ required: true }]}>
+        <Form.Item
+          name="serial"
+          label="시리얼넘버"
+          rules={[
+            { required: true, message: '시리얼넘버를 입력하세요.' },
+            { min: 3, max: 20, message: '시리얼넘버는 3자에서 20자 사이여야 합니다.' },
+          ]}
+        >
           <Input
             style={inputStyle}
             placeholder="시리얼넘버를 입력하세요."
             onChange={(e) => setSerial(e.target.value)}
           />
         </Form.Item>
-
         {/* 버스등록, 버스삭제, 취소 버튼 */}
         <Form.Item name="busCommit" style={{ marginTop: '45px', ...tailLayout, marginLeft: '100px' }}>
           <div>
@@ -78,11 +95,10 @@ const App: React.FC = () => {
             <Button style={{ ...buttonStyle }} htmlType="submit">
               버스등록
             </Button>
-
+            {/* 버스삭제 버튼 */}
             <Button style={{ ...buttonStyle, marginRight: '20px' }} onClick={() => router.push('/auth/busdelete')}>
               버스삭제
             </Button>
-
             {/* 취소 버튼 */}
             <Link href="/auth/buslist">
               <Button style={buttonStyle} htmlType="button" onClick={onReset}>
